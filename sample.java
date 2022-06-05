d4e5f60af15a6e9aa342d6b0fa433dd3fe810991
cgviak <104108988+cgviak@users.noreply.github.com>
2022-06-04 23:29:20 -0300

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,3 +1,4 @@
+// incluye solamente el back-end. Proyecto desarollado en Java debido a su seguridad.
 package hackathon;
 import java.util.*;
 public class Audit {
@@ -10,11 +11,11 @@ public class Audit {
 		
 		String auditoria, resultados;
 		
-			System.out.println ("Selecione una auditoría:");
+			System.out.println ("Selecione una auditoría:"); // entre la lista (linea 8)
 			auditoria = sc.nextLine();
 			
 			if (auditoria == "spid") {
-				int regs []	= {1, 2, 3, 4, 5};
+				int regs []	= {1, 2, 3, 4, 5}; // la cantidad de puntos regulatorios
 				for (int req : regs) {
 
 				System.out.println ("Cuál Requerimiento?");
@@ -23,18 +24,19 @@ public class Audit {
 			switch (req) {
 			case 1:
 				String comment;
-				System.out.println ("Texto de la regulación");
-				System.out.print ("La empresa cumple con el requisito?");
+				System.out.println ("Texto de la regulación"); // incluye el texto de la Regulación SPID punto 1
+				System.out.print ("La empresa cumple con el requisito?"); //Opción cumple o no cumple (linea 9)
 				resultados = sc.nextLine();
-				System.out.println ("Comentarios de Auditoría: ");
+				System.out.println ("Comentarios de Auditoría: "); // Auditor incluye sus papeles de trabajo.
 				comment = sc.nextLine ();
-				System.out.println ("Incluir anexo.");
+				System.out.println ("Incluir anexo."); // en el futuro, desarollaremos una herramienta para anexar archivos.
+					//incluir una tolerancia de riesgo menor (caso la empresa lo exige)... más espacio para papeles de trabajo
 				break;
 			}
 			}
-			} else if (auditoria == "SPEI") {
+			} else if (auditoria == "SPEI") { //solamente como proof-of-concept... seción sigue los mismos parametros.
 				
-			} else if (auditoria == "BDT") {
+			} else if (auditoria == "BDT") { //solamente como proof-of-concept... seción sigue los mismos parametros.
 				
 			} else {
 				System.out.print("valor invalido. no consta en nuestros sistemas.");
